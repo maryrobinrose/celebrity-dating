80be469216fd03a62051d187ecb098db2ef7a353
Alex Garcia and Mary McMurray <mary@>
2018-10-18 14:46:18 -0700

    add initial files

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(function() {
+  $("#bars").click(function(){
+    $("#bars").text
+   var allInputs = $("#bars".text);
+
+
+    if (allInputs = "*") {
+      $("#celeb").show()
+    }
+  });
+});
+
+
+
+
+  //  // $("#bars").(function(){
+  //   var celeb = $("#bars").text();
+  //   // console.log("#bars")
+  //   })
+
+
+    // var formChildren = $( "form > *" );
+
+
+    // $( "form" ).submit(function( event ) {
+    //   event.preventDefault();
