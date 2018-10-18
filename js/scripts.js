7204906c345ec3d3188752aa8aa47ff96333e89c
Alex Garcia and Mary McMurray <alex@>
2018-10-18 15:28:35 -0700

    add hide function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   $("form#match").submit(function(event) {
+
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
 
     if (age <= 100) {
       $("#celebrity").show();
-    }
-    if (age > 100) {
+      $("#celebrity2").hide();
+    } else {
+      $("#celebrity").hide();
       $("#celebrity2").show();
     }
 
